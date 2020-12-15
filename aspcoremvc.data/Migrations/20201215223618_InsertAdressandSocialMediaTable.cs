4aca0c7e878f93fd5c2f8fe1aa44ffe5c224beb0
mmertelibol <m.elibol@bilgeadam.com>
2020-12-16 01:39:57 +0300

    Migration alındı

---
+++ aspcoremvc.data/Migrations/20201215223618_InsertAdressandSocialMediaTable.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class InsertAdressandSocialMediaTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageUrl",
+                table: "Sliders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Href",
+                table: "Sliders",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Adresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddedDate = table.Column<DateTime>(nullable: true),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    AddedById = table.Column<int>(nullable: true),
+                    UpdatedById = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    County = table.Column<string>(nullable: true),
+                    FullAdress = table.Column<string>(nullable: true),
+                    Longitude = table.Column<string>(nullable: true),
+                    Latitude = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adresses_Users_AddedById",
+                        column: x => x.AddedById,
+                        principalSchema: "User",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Adresses_Users_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalSchema: "User",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialMedia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddedDate = table.Column<DateTime>(nullable: true),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    AddedById = table.Column<int>(nullable: true),
+                    UpdatedById = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    IsVisible = table.Column<string>(nullable: true),
+                    Icon = table.Column<string>(nullable: true),
+                    Href = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialMedia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SocialMedia_Users_AddedById",
+                        column: x => x.AddedById,
+                        principalSchema: "User",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SocialMedia_Users_UpdatedById",
+                        column: x => x.UpdatedById,
+                        principalSchema: "User",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adresses_AddedById",
+                table: "Adresses",
+                column: "AddedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adresses_UpdatedById",
+                table: "Adresses",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialMedia_AddedById",
+                table: "SocialMedia",
+                column: "AddedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialMedia_UpdatedById",
+                table: "SocialMedia",
+                column: "UpdatedById");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adresses");
+
+            migrationBuilder.DropTable(
+                name: "SocialMedia");
+
+            migrationBuilder.DropColumn(
+                name: "Href",
+                table: "Sliders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PageUrl",
+                table: "Sliders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
