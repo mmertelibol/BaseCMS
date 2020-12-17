4f5e8ec4ff8c42f8e01e8acc8236a92b5fc7f93d
mmertelibol <m.elibol@bilgeadam.com>
2020-12-17 03:29:42 +0300

    NewsCategory controller ve buna baglı index view i oluşturuldu.

---
+++ aspcoremvc/Controllers/NewsCategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class NewsCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult InsertNewsCategory()
+        {
+            return Json("true");
+        }
+
+        public JsonResult DeleteNewsCategory()
+        {
+            return Json("true");
+        }
+
+        public JsonResult UpdateNewsCategory()
+        {
+            return Json("true");
+        }
+
+        public JsonResult GetCategoryById()
+        {
+            return Json("true");
+        }
+    }
+}
